5af81eeabecb95a5904d1a42498fd574d8159c75
Samuel Atkins <sam@samatkins.co.uk>
2014-03-30 17:35:33 +0100

    Set-up a basic template

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return "Hello, world!"
 
+@app.route("/watch/<video_slug>")
+def watch(video_slug):
+	return render_template("watch.html", video_name=video_slug, scripts=["//vjs.zencdn.net/4.2/video.js"])
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
